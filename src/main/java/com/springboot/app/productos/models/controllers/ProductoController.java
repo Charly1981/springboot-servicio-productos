a29689394b0ca101d298172ceeb15f760433a518
Charly <charly81.zirpoli@gmail.com>
2022-11-14 18:09:44 -0300

    Se agrega resilience4j

--- src/main/java/com/springboot/app/productos/models/controllers/ProductoController.java
+++ src/main/java/com/springboot/app/productos/models/controllers/ProductoController.java
@@ -1,6 +1,7 @@
 package com.springboot.app.productos.models.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.productos.models.entity.Producto;
@@ -31,16 +31,29 @@ public class ProductoController {
 	public List<Producto> listar() {
 		return productoService.findAll().stream().map(producto -> {
 			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-			//producto.setPort(port);
+			// producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
 
 	@GetMapping("/ver/{id}")
 	public Producto detalle(@PathVariable Long id) {
+		if (id.equals(10L)) {
+			throw new IllegalStateException("Producto no encontrado!");
+		}
+
+		if (id.equals(7L)) {
+			try {
+				TimeUnit.SECONDS.sleep(5L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 		Producto producto = productoService.findById(id);
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		//producto.setPort(port);
+		// producto.setPort(port);
 
 //		try {
 //			Thread.sleep(2000L);
